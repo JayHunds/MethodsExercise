31b39e524593065449a69f4d6972e7c5ffd6e63a
Jayson Hundsrucker <hundsruckerjayson@gmail.com>
2024-02-16 08:55:17 -0800

    Created a calculator type method

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,28 +1,67 @@
-﻿namespace MethodsExercise
+﻿
+using System.ComponentModel;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your name:");
+            //Console.WriteLine("Enter your name:");
 
-            string UserName = Console.ReadLine();
+            //string UserName = Console.ReadLine();
 
-            Console.WriteLine($"Hello, {UserName}");
+            //Console.WriteLine($"Hello, {UserName}");
 
-            Console.WriteLine("What is your favorite color?");
+            //Console.WriteLine("What is your favorite color?");
 
-            String favoriteColor = Console.ReadLine();
+            //String favoriteColor = Console.ReadLine();
 
-            Console.WriteLine("What is your favorite animal?");
+            //Console.WriteLine("What is your favorite animal?");
 
-            string favoriteAnimal = Console.ReadLine();
+            //string favoriteAnimal = Console.ReadLine();
 
-            Console.WriteLine("What is your favorite song?");
+            //Console.WriteLine("What is your favorite song?");
 
-            string favoriteSong = Console.ReadLine();
+            //string favoriteSong = Console.ReadLine();
 
-            Console.WriteLine($"Hello {UserName}, your favorite color is {favoriteColor}, you want a {favoriteAnimal} and your favorite song is {favoriteSong}");
+            //Console.WriteLine($"Hello {UserName}, your favorite color is {favoriteColor}, you want a {favoriteAnimal} and your favorite song is {favoriteSong}");
+
+            //exercise 2
+
+            Console.WriteLine("What number to add:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me another number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = Sum(num1, num2);
+            Console.WriteLine($"The sum is : {sum}");
+
+            Console.WriteLine("What number to multiply:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me another number:");
+            int num2 = int.Parse(Console.ReadLine()) ;
+
+            int product = Multiply(num1, num2);
+            Console.WriteLine($"The product is: {product}");
+
+        }
+
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+
+
+
+        }
+
+
+        public static int Multiply(int x , int y )
+        {
+            return x * y;
         }
     }
+
 }
