97a7484788f86681fe71e5cb4232d0c5b369c1e3
Jayson Hundsrucker <hundsruckerjayson@gmail.com>
2024-02-16 08:04:46 -0800

    Created method logging and returning user inputs

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your name:");
+
+            string UserName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {UserName}");
+
+            Console.WriteLine("What is your favorite color?");
+
+            String favoriteColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+
+            string favoriteAnimal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite song?");
+
+            string favoriteSong = Console.ReadLine();
+
+            Console.WriteLine($"Hello {UserName}, your favorite color is {favoriteColor}, you want a {favoriteAnimal} and your favorite song is {favoriteSong}");
         }
     }
 }
